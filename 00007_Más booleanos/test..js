eb12498bb3171d4c0822d0b58c3cb46e214ed219
franco@mumuki.org <franco@mumuki.org>
2020-03-29 19:32:14 +0000

    Mumuki Export on 2020-03-29 19:32:14 +0000

--- 00007_Más booleanos/test..js
+++ 00007_Más booleanos/test..js
@@ -42,3 +42,20 @@ describe("", () => {
       assert(!esNumeroDeLaSuerte(15));
   })
 })
+
+// errores comunes, ver reglas de asistencia
+describe("esNumeroDeLaSuerte", () => {
+  let numeros = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  
+  it("no tiene los paréntesis mal puestos", () => {
+    assert.notDeepEqual(numeros.filter(esNumeroDeLaSuerte), [ 0, 2, 3, 4, 6, 8, 9, 10, 12, 14, 16, 18, 20 ]);  
+  })
+  
+  it("no tiene los argumentos de esMultiploDe invertidos", () => {
+    assert.notDeepEqual(numeros.filter(esNumeroDeLaSuerte), [ 1, 2, 3 ]);  
+  })
+  
+  it("no tiene los argumentos invertidos y los paréntesis mal puestos", () => {
+    assert.notDeepEqual(numeros.filter(esNumeroDeLaSuerte), [ -1, 1, 2, 3 ]);  
+  })
+})
