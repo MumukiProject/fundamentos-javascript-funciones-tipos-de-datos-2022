0b8375b8bfff53b5aad612e5cd00170a526b4720
franco@mumuki.org <franco@mumuki.org>
2020-04-15 18:00:59 +0000

    Mumuki Export on 2020-04-15 18:00:59 +0000

--- 00014_Operando strings/test..js
+++ 00014_Operando strings/test..js
@@ -1,11 +1,11 @@
 describe("", function() {
-  it(longitudNombreCompleto("Cosme", "Fulanito"), function() {
+  it('longitudNombreCompleto("Cosme", "Fulanito")', function() {
     assert.equal(longitudNombreCompleto("Cosme", "Fulanito"), 14);
   });
-  it(longitudNombreCompleto("John", "Snow"), function() {
+  it('longitudNombreCompleto("John", "Snow")', function() {
     assert.equal(longitudNombreCompleto("John", "Snow"), 9);
   });
-  it(longitudNombreCompleto("Juana", "Azurduy"), function() {
+  it('longitudNombreCompleto("Juana", "Azurduy")', function() {
     assert.equal(longitudNombreCompleto("Juana", "Azurduy"), 13);
   });
 })
