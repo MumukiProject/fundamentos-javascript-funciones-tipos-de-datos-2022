ad4be66f1b098def0e63339163ae00110634e58b
franco@mumuki.org <franco@mumuki.org>
2020-04-17 17:49:17 +0000

    Mumuki Export on 2020-04-17 17:49:17 +0000

--- 00006_Booleanos/test..js
+++ 00006_Booleanos/test..js
@@ -40,21 +40,22 @@ describe("", function() {
   });
 })
 
-describe("estaFueraDeRango", () => {
-  try {
-    let resultado = estaFueraDeRango(2, 0, 100)  === null || estaFueraDeRango(2, 0, 100)  === undefined || estaFueraDeRango(2, 0, 100)  === null || estaFueraDeRango(2, 0, 100)  === undefined;
-    it("devuelve siempre algo", () => {
-      assert(!resultado, "ojo, tenés que siempre devolver un valor booleano")  
-    })
-  } catch (e) {}
-})
-
 
 describe("estaEntre", () => {
   try {
-    let resultado = estaEntre(200, 54, 112)  === null || estaEntre(200, 54, 112)  === undefined || estaEntre(200, 54, 112)  === null || estaEntre(200, 54, 112)  === undefined;
+    let resultado = estaEntre(200, 54, 112)  === null || estaEntre(200, 54, 112)  === undefined || estaEntre(2, 0, 100)  === null || estaEntre(2, 0, 100)  === undefined;
     it("devuelve siempre algo", () => {
       assert(!resultado, "ojo, tenés que siempre devolver un valor booleano")  
     })
   } catch (e) {}
 })
+
+describe("estaFueraDeRango", () => {
+  try {
+    let resultado = estaFueraDeRango(2, 0, 100)  === null || estaFueraDeRango(2, 0, 100)  === undefined || estaFueraDeRango(12, 1, 10)  === null || estaFueraDeRango(12, 1, 10)  === undefined;
+    it("devuelve siempre algo", () => {
+      assert(!resultado, "ojo, tenés que siempre devolver un valor booleano")  
+    })
+  } catch (e) {}
+})
+
