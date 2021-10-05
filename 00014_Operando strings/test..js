20f2fdee215e985d25285499ed686d34a4d3823f
mayra@mumuki.org <mayra@mumuki.org>
2021-10-05 18:22:52 +0000

    Mumuki Export on 2021-10-05 18:22:52 +0000

--- 00014_Operando strings/test..js
+++ 00014_Operando strings/test..js
@@ -1,9 +1,9 @@
 describe("", function() {
-  it('longitudNombreCompleto("Cosme", "Fulanito")', function() {
-    assert.equal(longitudNombreCompleto("Cosme", "Fulanito"), 14);
+  it('longitudNombreCompleto("Max", "Power")', function() {
+    assert.equal(longitudNombreCompleto("Max", "Power"), 9);
   });
-  it('longitudNombreCompleto("John", "Snow")', function() {
-    assert.equal(longitudNombreCompleto("John", "Snow"), 9);
+  it('longitudNombreCompleto("Mercedes", "Sosa")', function() {
+    assert.equal(longitudNombreCompleto("Mercedes", "Sosa"),13 );
   });
   it('longitudNombreCompleto("Juana", "Azurduy")', function() {
     assert.equal(longitudNombreCompleto("Juana", "Azurduy"), 13);
