7862344372e20bbf994fffd0712eeb5a41067cef
gustavo@mumuki.org <gustavo@mumuki.org>
2021-12-09 12:24:28 +0000

    Mumuki Export on 2021-12-09 12:24:28 +0000

--- 00012_¿De qué signo sos_/test..js
+++ 00012_¿De qué signo sos_/test..js
@@ -2,12 +2,19 @@ describe("", function() {
   it("El signo de 10 es 1 por ser positivo", function() {
     assert.equal(signo(10), 1)
   });
+  
   it("El signo de 1 es 1 por ser positivo", function() {
     assert.equal(signo(1), 1)
-  })
+  });
+  
   it("El signo de 0 es 0", function() {
     assert.equal(signo(0), 0)
+  });
+  
+  it("El signo de -1 es -1 por ser negativo", function() {
+    assert.equal(signo(-1), -1)
   })
+  
   it("El signo de -65 es -1 por ser negativo", function() {
     assert.equal(signo(-65), -1)
   })
