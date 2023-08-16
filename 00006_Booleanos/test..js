10378ba6b4558170430814d60dc88abced034538
franco@mumuki.org <franco@mumuki.org>
2023-08-16 19:31:03 +0000

    Mumuki Export on 2023-08-16 19:31:03 +0000

--- 00006_Booleanos/test..js
+++ 00006_Booleanos/test..js
@@ -1,6 +1,6 @@
 describe("", function() {
-  it("estaEntre(10, 1, 10) es false", function() {
-    assert(estaEntre(10, 1, 10) === false);
+  it("estaEntre(10, 1, 9) es false", function() {
+    assert(estaEntre(10, 1, 9) === false);
   });
   it("estaEntre(4, 4, 9) es false", function() {
     assert(estaEntre(4, 4, 9) === false);
